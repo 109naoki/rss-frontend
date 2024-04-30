48aed56515b22bf03bc84eca3d49a6124caf029c
argyle-ito <ito@argyle.jp>
2024-04-30 15:10:06 +0900

    session

--- app/View/index.tsx
+++ app/View/index.tsx
@@ -2,9 +2,9 @@
 import { FC, useState } from "react";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { Modal } from "../components/Base/Modal";
-import { useAuthorizationHeaders } from "@/hooks/useAuthorizationHeaders";
 import { createItem } from "@/lib/api";
 import { AuthHeaders, Item, Zenn } from "@/type";
+import Skeleton from "@mui/material/Skeleton";
 
 type Props = {
   zenn: Zenn;
@@ -18,9 +18,9 @@ export const View: FC<Props> = ({ zenn, token, myItem: initialItem }) => {
   const closeModal = () => setIsOpen(false);
   const [myItems, setMyItems] = useState(initialItem);
 
-  const isBookmarked = (link: string) => {
-    return myItems.data.some((item: Item) => item.link === link);
-  };
+  const isBookmarked = (link: string) =>
+    myItems.data.some((item: Item) => item.link === link);
+
   const handleBookmark = async (item: Item) => {
     const itemData = {
       name: item.title,
@@ -44,6 +44,7 @@ export const View: FC<Props> = ({ zenn, token, myItem: initialItem }) => {
       console.error("Error handling the bookmark:", error);
     }
   };
+
   return (
     <>
       <div className="container mx-auto px-4">
@@ -58,61 +59,71 @@ export const View: FC<Props> = ({ zenn, token, myItem: initialItem }) => {
           </p>
         </Modal>
         <div className="flex flex-wrap -mx-2 mt-12">
-          {zenn.items.map((item, index) => (
-            <div key={index} className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
-              <div className="border border-gray-200 rounded-lg overflow-hidden hover:shadow-xl transition-all duration-500 ease-in-out transform hover:scale-105">
-                <a
-                  href={item.link}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="p-4 block"
-                >
-                  {item.title && (
-                    <p className="text-lg font-semibold mb-2">
-                      {item.title.length > 40
-                        ? item.title.substring(0, 40) + "..."
-                        : item.title}
-                    </p>
-                  )}
-                  {item.enclosure && item.enclosure.url && (
-                    <img
-                      src={item.enclosure.url}
-                      alt="記事の画像"
-                      className="w-full mb-2"
-                    />
-                  )}
-                  <div className="flex items-center justify-between">
-                    <p className="text-sm text-gray-600">
-                      {new Date(item.isoDate).toLocaleDateString("ja-JP")}
-                    </p>
-                    {token ? (
-                      <BookmarkIcon
-                        className={"size-8 cursor-pointer"}
-                        onClick={(e) => {
-                          e.preventDefault();
-                          handleBookmark(item);
-                        }}
-                        sx={{
-                          color: isBookmarked(item.link) ? "red" : "inherit",
-                        }}
-                      />
-                    ) : (
-                      <BookmarkIcon
-                        className="size-8 cursor-pointer"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          openModal();
-                        }}
-                        sx={{
-                          color: "inherit",
-                        }}
-                      />
-                    )}
+          {!zenn.items.length
+            ? Array.from(new Array(6)).map((_, index) => (
+                <div key={index} className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
+                  <Skeleton variant="rectangular" height={118} />
+                  <Skeleton variant="text" />
+                  <Skeleton variant="text" />
+                </div>
+              ))
+            : zenn.items.map((item, index) => (
+                <div key={index} className="w-full sm:w-1/2 md:w-1/3 px-2 mb-4">
+                  <div className="border border-gray-200 rounded-lg overflow-hidden hover:shadow-xl transition-all duration-500 ease-in-out transform hover:scale-105">
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="p-4 block"
+                    >
+                      {item.title && (
+                        <p className="text-lg font-semibold mb-2">
+                          {item.title.length > 40
+                            ? item.title.substring(0, 40) + "..."
+                            : item.title}
+                        </p>
+                      )}
+                      {item.enclosure && item.enclosure.url && (
+                        <img
+                          src={item.enclosure.url}
+                          alt="記事の画像"
+                          className="w-full mb-2"
+                        />
+                      )}
+                      <div className="flex items-center justify-between">
+                        <p className="text-sm text-gray-600">
+                          {new Date(item.isoDate).toLocaleDateString("ja-JP")}
+                        </p>
+                        {token ? (
+                          <BookmarkIcon
+                            className={"size-8 cursor-pointer"}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              handleBookmark(item);
+                            }}
+                            sx={{
+                              color: isBookmarked(item.link)
+                                ? "red"
+                                : "inherit",
+                            }}
+                          />
+                        ) : (
+                          <BookmarkIcon
+                            className="size-8 cursor-pointer"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              openModal();
+                            }}
+                            sx={{
+                              color: "inherit",
+                            }}
+                          />
+                        )}
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
-            </div>
-          ))}
+                </div>
+              ))}
         </div>
       </div>
     </>
